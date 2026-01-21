8cb8123943747ded478e17eac5cca565b838436b
javier <jriveradelgado3@gmail.com>
2026-01-21 19:29:45 +0100

    Segundo commit

---
+++ java/src/Usuarios.java
@@ -0,0 +1,27 @@
+//@author Paco Zahinos Duarte
+//@author Javier Rivera Delgado
+public class Usuarios {
+
+    private String nombre;
+    private String password;
+    private String rol; // "ADMIN" o "USUARIO"
+
+    public Usuarios(String nombre, String password, String rol) {
+        this.nombre = nombre;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario: " + nombre + " | Rol: " + rol;
+    }
+}
