9e6649475ecde8b12453f98911c1d7aa484a5b21
Paco <fazahinosd01@iesalbarregas.es>
2026-01-23 18:34:39 +0100

    get y set libros

--- java/src/Libros.java
+++ java/src/Libros.java
@@ -5,10 +5,29 @@ public class Libros {
     private String autor;
     private int id;
 
-    
-
+    public Libros(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.id = generadId();
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public int getId() {
+        return id;
+    }
     private int generadId(){
         Random rd = new Random();
+        
         
     }
 }
